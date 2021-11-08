bbdae5cc39058564cff0b94aa4d903749ac89efb
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2021-11-08 18:52:15 +0300

    form complate

--- #src/js/main.js
+++ #src/js/main.js
@@ -133,17 +133,6 @@ class DoubleSlider {
 
 
 
-const orderLabel = document.querySelectorAll(".checkbox__item")
-
-orderLabel.forEach(item=>{
-  item.addEventListener("change",()=>{
-      item.classList.toggle("active")
-  })
-})
-
-
-
-
 const { to, set, from, fromTo } = gsap
 
 const getVar = (key, elem = document.documentElement) => getComputedStyle(elem).getPropertyValue(key)
@@ -151,11 +140,19 @@ const getVar = (key, elem = document.documentElement) => getComputedStyle(elem).
 document.querySelectorAll('.price__estimate').forEach(elem => {
     let svg = elem.querySelector('svg'),
         input = elem.querySelector('input')
+		elem.addEventListener("change",()=>{
+			if(input.checked){
+				input.setAttribute("value", "Так")
+			}else if(!input.checked){
+				input.removeAttribute("value")
+			}
+		})
     input.addEventListener('change', e => {
         let checked = input.checked
         if(!checked) {
             return
         }
+		
         fromTo(input, {
             '--border-width': '3px'
         }, {
@@ -311,5 +308,137 @@ function changeActive(e) {
 	$(e).addClass('active');
 }
 
+
+
+
+
+const section = document.querySelectorAll(".animate")
+
+window.addEventListener("scroll",()=>{
+	const margeTop = window.scrollY
+	section.forEach(item=>{
+		const sectionTop = item.offsetTop - 400
+		if(margeTop < sectionTop){
+			item.classList.remove("test")
+		}else if(!item.classList.contains("test") && margeTop > sectionTop){
+			item.classList.add("test")
+		}
+})
+})
+
+const orderLabel = document.querySelectorAll(".checkbox__item")
+const arr = []
+
+orderLabel.forEach(item=>{
+  item.addEventListener("change",()=>{
+      item.classList.toggle("active")
+	  arr.push( item.querySelector("input").value)
+  })
+})
+
+$('.order__form').on('submit', function (event) {
+
+    event.stopPropagation();
+    event.preventDefault();
+
+    let form = this,
+        submit = $('.submit', form),
+        data = new FormData(),
+        files = $('input[type=file]')
+
+
+    $('.submit', form).val('Отправка...');
+    $('input, textarea', form).attr('disabled','');
+
+
+    data.append( 'Ім\'я', 			$('[name="user_name"]', form).val() );
+    data.append( 'Email', 			$('[name="user_email"]', form).val() );
+    data.append( 'Повідомлення',	$('[name="user_message"]', form).val() );
+    data.append( 'Файл', 			$('[name="file"]', form).val() );
+	data.append('Послуги', arr);
+	data.append("Мінімальний Бютжет", $('[name="minPrice"]', form).val() + '$' );
+	data.append("Максимальний Бютжет", $('[name="maxprice"]', form).val() + '$');
+	data.append("Потрібен Прорахунок", $('#estimate', form).val());
+	data.append("White Label", $('[name="Whitelabel"]', form).val());
+	data.append("NDA", $('[name="NDA"]', form).val());
+
+
+   
+
+    files.each(function (key, file) {
+        let cont = file.files;
+        if ( cont ) {
+            $.each( cont, function( key, value ) {
+                data.append( key, value );
+            });
+        }
+    });
+    
+    $.ajax({
+        url: './index.php',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        xhr: function() {
+            let myXhr = $.ajaxSettings.xhr();
+
+            if ( myXhr.upload ) {
+                myXhr.upload.addEventListener( 'progress', function(e) {
+                    if ( e.lengthComputable ) {
+                        let percentage = ( e.loaded / e.total ) * 100;
+                            percentage = percentage.toFixed(0);
+                        $('.submit', form)
+                            .html( percentage + '%' );
+                    }
+                }, false );
+            }
+
+            return myXhr;
+        },
+        error: function( jqXHR, textStatus ) {
+            // Тут выводим ошибку
+        },
+        complete: function() {
+            // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+            console.log('Complete')
+			formPreview.innerHTML = ''
+			$('.checkbox__item').removeClass("active")
+            form.reset()
+			window.location = '/thanks.html';
+        }
+    });
+
+    return false;
+});
+
+
+
+
+const formImg = document.querySelector(".fileImg")
+const formPreview = document.getElementById("filePreview")
+
+
+
+formImg.addEventListener("change",()=>{
+	uploadFile(formImg.files[0]);
+})
+
+function uploadFile(file){
+	var reader = new FileReader();
+	reader.onload = function(e){
+		if(file.type.startsWith('application/pdf')){
+			formPreview.innerHTML = `<img src="https://cdn.iconscout.com/icon/free/png-256/pdf-2752103-2284920.png" alt="Preview">  <p>${file.name}</p>`
+		}else if(file.type.startsWith('image/')){
+			formPreview.innerHTML = `<img src="${e.target.result}" alt="Preview"> <p>${file.name}</p>`
+		}
+	}
+	reader.onerror = function(e){
+		alert(this.error)
+	}
+	reader.readAsDataURL(file);
+}
 
 new CostForm(document.querySelector(".cost"));
